044c0d0b84d4d39978bac5c54dcb6ac1f8dca544
Jialin Wu <31950794+Jialin-Wu@users.noreply.github.com>
2018-06-05 14:30:27 +0800

    Add files via upload

---
+++ osupgrade.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove
+
+echo ""
+echo ""
+echo "	 ======================================= "
+echo "	 OS upgrading complete, have a nice day! "
+echo "	 ======================================= "
+echo ""
+echo ""
